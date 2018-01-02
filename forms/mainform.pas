e0920a11c75150c7d0cb451ea5d00397019cd9ae
Mykola Morhun <Nicksbox78@gmail.com>
2018-01-02 21:38:52 +0200

    Application menu prototype

--- forms/mainform.pas
+++ forms/mainform.pas
@@ -6,25 +6,49 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics,
-  Dialogs, StdCtrls,
-  WpcExceptions,
-  WpcScript,
-  WpcScriptParser,
-  WpcScriptExecutor,
-  WallpaperSetter;
+  Dialogs, ExtCtrls, Menus, ExtDlgs;
 
 type
 
   { TBannerForm }
 
   TBannerForm = class(TForm)
-    Button1: TButton;
-    OpenDialog1: TOpenDialog;
-    procedure Button1Click(Sender: TObject);
-  private
-    { private declarations }
-  public
-    { public declarations }
+    WPCTrayIcon: TTrayIcon;
+    SelectScriptDialog: TOpenDialog;
+    SelectWallpaperDirectoryDialog: TSelectDirectoryDialog;
+    SelectWallpaperDialog: TOpenPictureDialog;
+    MainContextMenuImageList: TImageList;
+    WPCMainPopupMenu: TPopupMenu;
+    StopMenuItem: TMenuItem;
+    ScriptMenuItem: TMenuItem;
+    RunScriptMenuItem: TMenuItem;
+    ScriptEditorMenuItem: TMenuItem;
+    WallpaperMenuItem: TMenuItem;
+    SetWallpaperDirectoryMenuItem: TMenuItem;
+    PreviousWallpaperMenuItem: TMenuItem;
+    NextWallpaperMenuItem: TMenuItem;
+    Separator2MenuItem: TMenuItem;
+    SetWallpaperImageMenuItem: TMenuItem;
+    Separator1MenuItem: TMenuItem;
+    OptionsMenuItem: TMenuItem;
+    InfoMenuItem: TMenuItem;
+    DocsMenuItem: TMenuItem;
+    AboutMenuItem: TMenuItem;
+    ExitMenuItem: TMenuItem;
+
+    procedure FormCreate(Sender: TObject);
+
+    procedure StopMenuItemClick(Sender: TObject);
+    procedure RunScriptMenuItemClick(Sender: TObject);
+    procedure ScriptEditorMenuItemClick(Sender: TObject);
+    procedure SetWallpaperDirectoryMenuItemClick(Sender: TObject);
+    procedure NextWallpaperMenuItemClick(Sender: TObject);
+    procedure PreviousWallpaperMenuItemClick(Sender: TObject);
+    procedure SetWallpaperImageMenuItemClick(Sender: TObject);
+    procedure OptionsMenuItemClick(Sender: TObject);
+    procedure DocsMenuItemClick(Sender: TObject);
+    procedure AboutMenuItemClick(Sender: TObject);
+    procedure ExitMenuItemClick(Sender: TObject);
   end;
 
 var
@@ -34,61 +58,72 @@ implementation
 
 {$R *.lfm}
 
-procedure RunScript(PathToScript : String);
-var
-  ScriptContent   : TStringList;
-  ScriptParser    : TWpcScriptParser;
-  Script          : TWpcScript;
-  ScriptExecutor  : TWpcScriptExecutor;
-  WallpaperSetter : IWallpaperSetter;
-
-  ErrorMessage : String;
-begin
-    try
-      try
-        ScriptContent := TStringList.Create();
-        // TODO check file type and size (limit into property)
-        ScriptContent.LoadFromFile(PathToScript);
-        ScriptParser := TWpcScriptParser.Create(ScriptContent);
-        Script := ScriptParser.Parse();
-        WallpaperSetter := TWpcDebugWallpaperSetter.Create('log.txt');
-        ScriptExecutor := TWpcScriptExecutor.Create(Script, WallpaperSetter);
-        ScriptExecutor.ExecuteScript();
-      except
-        on ParseExcepton : TWpcScriptParseException do begin
-          ErrorMessage := Concat('Failed to parse script: ', ParseExcepton.Message);
-          if (ParseExcepton.Line <> TWpcScriptParseException.UNKNOWN_LINE) then
-            ErrorMessage := Concat(ErrorMessage, ' Line: ', IntToStr(ParseExcepton.Line + 1));
-          if (ParseExcepton.WordNumer <> TWpcScriptParseException.UNKNOWN_WORD_NUMBER) then
-            ErrorMessage := Concat(ErrorMessage, ' Word: ', IntToStr(ParseExcepton.WordNumer + 1));
-          WriteLn(ErrorMessage);
-        end;
-        on WpcException : TWpcException do begin
-          ErrorMessage := Concat('Error: ', WpcException.Message);
-          WriteLn(ErrorMessage);
-        end;
-        on E : Exception do begin
-          ErrorMessage := Concat('Unknown error: ', E.Message);
-          WriteLn(ErrorMessage);
-        end;
-      end;
-    finally
-      {if (ScriptParser <> nil) then ScriptParser.Free();
-      if (ScriptExecutor <> nil) then ScriptExecutor.Free();
-      if (WallpaperSetter <> nil) then WallpaperSetter.Free(); }
-      ScriptContent.Free();
-    end;
-end;
-
 { TBannerForm }
 
-procedure TBannerForm.Button1Click(Sender: TObject);
-begin
-  if OpenDialog1.Execute() then
-    RunScript(OpenDialog1.Filename);
+(* Application initialization *)
 
-  WriteLn('The Script exits without erros.');
+procedure TBannerForm.FormCreate(Sender: TObject);
+begin
+  WPCTrayIcon.Show();
 end;
+
+(* Application menu handlers *)
+
+procedure TBannerForm.StopMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TBannerForm.RunScriptMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TBannerForm.ScriptEditorMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TBannerForm.SetWallpaperDirectoryMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TBannerForm.NextWallpaperMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TBannerForm.PreviousWallpaperMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TBannerForm.SetWallpaperImageMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TBannerForm.OptionsMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TBannerForm.DocsMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TBannerForm.AboutMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TBannerForm.ExitMenuItemClick(Sender: TObject);
+begin
+  BannerForm.Close();
+end;
+
 
 end.
 
